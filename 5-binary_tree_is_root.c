ded6d2114281570e2fea884e9cafa723bf73c9b8
fim4re <fimareyassine@gmail.com>
2024-03-26 08:37:04 -0700

    commit for the task

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - checks if a node is root
+ * @node: Node
+ * Return: 0 if is not a root 1 if it is
+*/
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node != NULL || node->parent == NULL)
+		return (1);
+	return (0);
+}
