f97c280712efdea7bd23542e80dc6f6f4ace4d7e
fim4re <fimareyassine@gmail.com>
2024-03-27 10:15:41 -0700

    commit for the task

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -11,7 +11,11 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	int left = 0, right = 0;
 
-	if (tree)
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
 	{
 		if (!tree->left && !tree->right)
 		{
@@ -26,12 +30,10 @@ int binary_tree_is_full(const binary_tree_t *tree)
 			left = binary_tree_is_full(tree->left);
 			right = binary_tree_is_full(tree->right);
 			if (left == 0 || right == 0)
+			{
 				return (0);
+			}
 			return (1);
 		}
-	}
-	else
-	{
-		return (0);
 	}
 }
