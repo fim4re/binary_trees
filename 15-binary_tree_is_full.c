40e898870920c0b7decb108ff0c0d7486b6389a2
fim4re <fimareyassine@gmail.com>
2024-03-27 10:34:24 -0700

    commit for the task

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,39 +1,35 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_is_full - if binary tree is full.
- * @tree: pointer root
- *
- * Return: If tree is NULL or is not full - 0.
- *         Otherwise - 1.
-*/
+ * binary_tree_is_full - function that says if a tree is full or not
+ * a tree is full if it has tow or none children
+ * @tree: tree to check
+ * Return: 1 if it is a full 0 if not
+ */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int left = 0, right = 0;
+	int lt = 0, rt = 0;
 
 	if (tree == NULL)
-	{
 		return (0);
-	}
 	else
 	{
-		if (!tree->left && !tree->right)
+		if (tree->left && tree->right)
+		{
+			lt = binary_tree_is_full(tree->left);
+			rt = binary_tree_is_full(tree->right);
+			if (left == 0 || right == 0)
+			{
+				return (0);
+			}
+			return (1);
+		}
+		else if (!tree->left && !tree->right)
 		{
 			return (1);
 		}
 		else
 		{
 			return (0);
-		}
-		else if (tree->left && tree->right)
-		{
-			left = binary_tree_is_full(tree->left);
-			right = binary_tree_is_full(tree->right);
-			if (left == 0 || right == 0)
-			{
-				return (0);
-			}
-			return (1);
 		}
 	}
 }
